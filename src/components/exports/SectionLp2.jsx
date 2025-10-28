765237821c85ae2b867fe5c1b3a759226f075c2c
mircomania <mirco.rodriguez@hotmail.com>
2025-10-28 18:31:45 -0300

    3 commit flip inmobiliaria - section 3 1.0

--- src/components/exports/SectionLp2.jsx
+++ src/components/exports/SectionLp2.jsx
@@ -7,18 +7,20 @@ import { NavLink } from 'react-router-dom';
 export const SectionLp2 = () => {
     return (
         <section className={styles.sectionContainer}>
-            <header>
-                <h2 className="formula-bold">Nuestros servicios</h2>
-            </header>
+            <div className={styles.sectionContenido}>
+                <header>
+                    <h2 className="formula-bold">Nuestros servicios</h2>
+                </header>
 
-            <div className={styles.serviciosContainer}>
-                {serviciosSectionLp2.map((servicio) => (
-                    <NavLink key={servicio.id} className={styles.servicio}>
-                        <h3 className="alliance-text">{servicio.titulo}</h3>
+                <div className={styles.serviciosContainer}>
+                    {serviciosSectionLp2.map((servicio) => (
+                        <NavLink key={servicio.id} className={styles.servicio}>
+                            <h3 className="alliance-text">{servicio.titulo}</h3>
 
-                        <p className="alliance-text">{servicio.texto}</p>
-                    </NavLink>
-                ))}
+                            <p className="alliance-text">{servicio.texto}</p>
+                        </NavLink>
+                    ))}
+                </div>
             </div>
         </section>
     );
